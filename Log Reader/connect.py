65b2de6cc33ddf8cfa91694eca25d84c9fb10942
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-28 14:41:45 +0530

    input keyevent
    https://gist.github.com/arjunv/2bbcca9a1a1c127749f8dcb6d36fb0bc

---
+++ Log Reader/connect.py
@@ -0,0 +1,46 @@
+
+# pip install pure-python-adb
+# https://gist.github.com/arjunv/2bbcca9a1a1c127749f8dcb6d36fb0bc
+# https://itnext.io/how-you-can-control-your-android-device-with-python-45c3ab15e260
+
+from ppadb.client import Client as AdbClient
+import time
+
+def connect():
+    client = AdbClient(host="127.0.0.1", port=5037) # Default is "127.0.0.1" and 5037
+    devices = client.devices()
+
+    if len(devices) == 0:
+        print('No devices')
+        quit()
+
+    device = devices[0]
+    print(f'Connected to {device}')
+    return device, client
+
+
+if __name__ == '__main__':
+    device, client = connect()
+
+    # open up camera app
+    device.shell('input keyevent 27')
+
+    # wait 5 seconds
+    time.sleep(5)
+
+    # take a photo with volume up
+    device.shell('input keyevent 24')
+
+    print('Taken a photo!')
+    time.sleep(5)
+
+    # # '26' is for power button
+    # device.shell('input keyevent 26')
+    
+
+'''
+>>> python connect.py
+
+Connected to <ppadb.device.Device object at 0x00000208B219F610>
+Taken a photo!
+'''
