c1dc488fdd48771a4679ed349c1cb41b768889e2
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-28 16:32:57 +0530

    ss set env for adb

--- Automate Android/connect.py
+++ Automate Android/connect.py
@@ -3,6 +3,7 @@
 # https://gist.github.com/arjunv/2bbcca9a1a1c127749f8dcb6d36fb0bc
 # https://itnext.io/how-you-can-control-your-android-device-with-python-45c3ab15e260
 
+
 from ppadb.client import Client as AdbClient
 import time
 
@@ -18,10 +19,27 @@ def connect():
     print(f'Connected to {device}')
     return device, client
 
+device, client = connect()
 
-if __name__ == '__main__':
-    device, client = connect()
 
+def browser_search():
+    search_bar = '440 200' # x y
+
+    query = input('What word do you want to find the definition of: ')
+    search_query = f'what is the definition of {query}'
+
+    device.shell('input keyevent 64')
+    time.sleep(0.25) # wait for browser to load
+    device.shell(f'input tap {search_bar}')
+
+    # make sure you have the quotation marks around your text
+    device.shell(f'input text "{search_query}"')
+
+    device.shell('input keyevent 66')
+    time.sleep(3) # wait for results to load
+
+
+def click_photo():
     # open up camera app
     device.shell('input keyevent 27')
 
@@ -30,13 +48,31 @@ if __name__ == '__main__':
 
     # take a photo with volume up
     device.shell('input keyevent 24')
-
+    
     print('Taken a photo!')
     time.sleep(5)
 
-    # # '26' is for power button
-    # device.shell('input keyevent 26')
-    
+
+def screen_shot():
+    screenshot = device.screencap()
+    filename = time.time()
+
+    # save the screenshot
+    with open(f'screenshots/{filename}.png', 'wb') as f:
+        f.write(screenshot)
+        print('Saved screenshot!')
+
+    time.sleep(5)
+
+
+if __name__ == '__main__':
+    click_photo()
+    # browser_search()
+    screen_shot()
+
+    # '26' is for power button
+    device.shell('input keyevent 26')
+
 
 '''
 python connect.py
